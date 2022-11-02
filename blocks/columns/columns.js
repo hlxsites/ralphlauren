5fe1056261434645cec77dcbd6bd68b9caa43688
Dirk Rudolph <drudolph@adobe.com>
2022-11-02 13:25:33 +0100

    feat: styling more sections

--- blocks/columns/columns.js
+++ blocks/columns/columns.js
@@ -1,23 +1,22 @@
 export default function decorate(block) {
   const cols = [...block.firstElementChild.children];
   block.classList.add(`columns-${cols.length}-cols`);
-  
+
   const columnsWrapper = block.parentElement;
   const columnsWrappers = columnsWrapper.parentElement.querySelectorAll('.columns-wrapper');
-  const index = [...columnsWrappers].indexOf(columnsWrapper);
-  columnsWrapper.classList.add(`columns-wrapper--${index + 1}`);
+  const columnsWrapperIndex = [...columnsWrappers].indexOf(columnsWrapper);
+  columnsWrapper.classList.add(`columns-wrapper-${columnsWrapperIndex + 1}`);
 
   cols.forEach((col, index) => {
-    col.classList.add(`column--${index + 1}`)
+    col.classList.add(`column-${index + 1}`);
     const hasText = !!col.querySelector('p,h1,h2,h3,h4');
     const hasImage = !!col.querySelector('picture');
     if (hasText && hasImage) {
-      col.classList.add('column--mixed');
+      col.classList.add('column-mixed');
     } else if (hasText) {
-      col.classList.add('column--text-only');
+      col.classList.add('column-text-only');
     } else if (hasImage) {
-      col.classList.add('column--image-only');
+      col.classList.add('column-image-only');
     }
-  })
-
+  });
 }
